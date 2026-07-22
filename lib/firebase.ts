11b6d5c54d44a331a5561eb48bce4fb10d391e13
hayato iwakura <s2510024@wakayama-u.ac.jp>
2026-07-22 13:01:28 +0900

    Refactor Firebase config to use environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAuAsWLctHoBp6Eh2UN2sIM3wE1YJZIxvQ",
-  authDomain: "shortcut-key-exam.firebaseapp.com",
-  projectId: "shortcut-key-exam",
-  storageBucket: "shortcut-key-exam.firebasestorage.app",
-  messagingSenderId: "522459442819",
-  appId: "1:522459442819:web:3a390baf5d2b6e001075b6",
-  measurementId: "G-7TRX1BYWC2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
